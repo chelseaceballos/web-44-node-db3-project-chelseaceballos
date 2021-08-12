be8e3583d00c54215c460f77a01e2b85daba00b9
Chelsea Ceballos <chelsea-ceballos@lambdastudents.com>
2021-08-11 19:47:46 -0700

    power keeps going out, will continue tomorrow '60:00'

--- api/schemes/scheme-model.js
+++ api/schemes/scheme-model.js
@@ -28,29 +28,21 @@ function find() { // EXERCISE A
 
 }
 
-function findById(scheme_id) { // EXERCISE B
+async function findById(scheme_id) { // EXERCISE B
   /*
     1B- Study the SQL query below running it in SQLite Studio against `data/schemes.db3`:
 
-      SELECT
-          sc.scheme_name,
-          st.*
-      FROM schemes as sc
-      LEFT JOIN steps as st
-          ON sc.scheme_id = st.scheme_id
-      WHERE sc.scheme_id = 1
-      ORDER BY st.step_number ASC;
-
+    
     2B- When you have a grasp on the query go ahead and build it in Knex
     making it parametric: instead of a literal `1` you should use `scheme_id`.
-
+    
     3B- Test in Postman and see that the resulting data does not look like a scheme,
     but more like an array of steps each including scheme information:
-
-      [
-        {
-          "scheme_id": 1,
-          "scheme_name": "World Domination",
+    
+    [
+      {
+        "scheme_id": 1,
+        "scheme_name": "World Domination",
           "step_id": 2,
           "step_number": 1,
           "instructions": "solve prime number theory"
@@ -64,10 +56,10 @@ function findById(scheme_id) { // EXERCISE B
         },
         // etc
       ]
-
-    4B- Using the array obtained and vanilla JavaScript, create an object with
-    the structure below, for the case _when steps exist_ for a given `scheme_id`:
-
+      
+      4B- Using the array obtained and vanilla JavaScript, create an object with
+      the structure below, for the case _when steps exist_ for a given `scheme_id`:
+      
       {
         "scheme_id": 1,
         "scheme_name": "World Domination",
@@ -85,16 +77,45 @@ function findById(scheme_id) { // EXERCISE B
           // etc
         ]
       }
-
-    5B- This is what the result should look like _if there are no steps_ for a `scheme_id`:
-
+      
+      5B- This is what the result should look like _if there are no steps_ for a `scheme_id`:
+      
       {
         "scheme_id": 7,
         "scheme_name": "Have Fun!",
         "steps": []
       }
+
+      SELECT
+          sc.scheme_name,
+          st.*
+      FROM schemes as sc
+      LEFT JOIN steps as st
+          ON sc.scheme_id = st.scheme_id
+      WHERE sc.scheme_id = 1
+      ORDER BY st.step_number ASC;
+
+
   */
-}
+
+const rows = await db('schemes as sc')
+ .leftJoin('steps as st', 'sc.scheme_id', "st.scheme_id")
+ .where('sc.scheme_id', scheme_id)
+ .select('st.*', 'sc.scheme_name')
+ .orderBy('st.step_number')
+ 
+    const result = {
+      scheme_id: rows[0].scheme_id,
+      scheme_name: rows[0].scheme_name,
+      steps: []
+    }    
+    rows.forEach(row => {
+      if(row.step_id) {
+        result.steps.push(row)
+      } 
+    })
+    return result
+};
 
 function findSteps(scheme_id) { // EXERCISE C
   /*

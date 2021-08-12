6b27b12273f6508c286c5322c4e6ffadf78c8633
Chelsea Ceballos <chelsea-ceballos@lambdastudents.com>
2021-08-11 17:19:51 -0700

    validateStep fixed NaN to 'number'

--- api/schemes/scheme-middleware.js
+++ api/schemes/scheme-middleware.js
@@ -38,23 +38,15 @@ if (
 }
 }
 
-/*
-  If `instructions` is missing, empty string or not a string, or
-  if `step_number` is not a number or is smaller than one:
 
-  status 400
-  {
-    "message": "invalid step"
-  }
-*/
 const validateStep = (req, res, next) => {
   const {instructions, step_number} = req.body
  
    if(
-     instructions === undefined ||
+     instructions === undefined || 
      typeof instructions !== "string" ||
      !instructions.trim() ||
-     typeof step_number !== NaN || // "number" or NaN??
+     typeof step_number !== "number" || //  NaN == not a number
      step_number < 1
    ) {
     next({ 
